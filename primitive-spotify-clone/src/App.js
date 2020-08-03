18b3191b95987494150653ed77a579bf8927f2a7
const.kamil.lazarski@gmail.com <const.kamil.lazarski@gmail.com>
2020-08-03 22:09:29 +0200

    it was installed spotify-web-api-js pack

--- primitive-spotify-clone/src/App.js
+++ primitive-spotify-clone/src/App.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Login from './Login';
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from "spotify-web-api-js";
 
-function App() {
+const spotify = new SpotifyWebApi();
+
+const App = () => {
+
+const [token, setToken] = useState(null);
+
+useEffect(() => {
+  const hash = getTokenFromUrl();
+  window.location.hash = "";
+  const _token = hash.access_token;
+
+  if (_token) {
+    setToken(_token);
+    spotify.setAccessToken(_token);
+    spotify.getMe().then(user => {
+      console.log("hey", user);
+    });
+  }
+
+}, []);
+
   return (
     <div className="app">
-      
-      {/* Spotify logo */}
-      {/* Login with Spotify button */}
-    <Login />  
-
+    { token ? <h1>I am logged in</h1> : <Login /> }
     </div>
   );
 }
--- primitive-spotify-clone/src/spotify.js
+++ primitive-spotify-clone/src/spotify.js
@@ -1,4 +1,3 @@
-
 export const authEndpoint = 
 "https://accounts.spotify.com/authorize";
 
@@ -12,5 +11,19 @@ const scopes = [
     "user-top-read",
     "user-modify-playback-state",
 ];
+
+export const getTokenFromUrl = () => {
+    return window.location.hash
+        .substring(1)
+        .split('&')
+        .reduce((initial, item) => {
+            let parts = item.split('=');
+            initial[parts[0]] = 
+            decodeURIComponent(parts[1]);
+
+            return initial;
+
+        }, {})
+};
 
 export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
