70dc7f15eda4fd61bcbcd0af54898994f69a4383
const.kamil.lazarski@gmail.com <const.kamil.lazarski@gmail.com>
2020-08-03 23:30:01 +0200

    It was set Context API as Data Layer

--- primitive-spotify-clone/src/App.js
+++ primitive-spotify-clone/src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import Login from './Login';
 import { getTokenFromUrl } from './spotify';
 import SpotifyWebApi from "spotify-web-api-js";
+import Player from './Player';
+import { useDataLayerValue } from './DataLayer';
 
 const spotify = new SpotifyWebApi();
 
 const App = () => {
-
-const [token, setToken] = useState(null);
+const [{user, token}, dispatch ] = useDataLayerValue();
 
 useEffect(() => {
   const hash = getTokenFromUrl();
@@ -16,18 +17,32 @@ useEffect(() => {
   const _token = hash.access_token;
 
   if (_token) {
-    setToken(_token);
+    
+    dispatch({ 
+      type: 'SET_TOKEN',
+      token: _token,
+    });
+
     spotify.setAccessToken(_token);
     spotify.getMe().then(user => {
-      console.log("hey", user);
+      dispatch({
+        type: 'SET_USER',
+        user: user,
+      });
     });
   }
 
 }, []);
 
-  return (
+console.log("user name:", user);
+console.log("token", token);
+
+
+return (
     <div className="app">
-    { token ? <h1>I am logged in</h1> : <Login /> }
+    { token ? 
+    <Player /> : 
+    <Login /> }
     </div>
   );
 }
---
+++ primitive-spotify-clone/src/reducer.js
@@ -0,0 +1,28 @@
+export const initialState = {
+    user: null,
+    playlists: [],
+    playing: false,
+    item: null,
+    token: 'BQAX5gUtI8NY5xXP0DzVheQHM0Vy4Y4BiX58dYoBia3fQesz_ubnjiMIxwm6PnJnZXTo4vC1UaDvFnSXp_Gh2tUjpFP6qoR83N6iHMOdOxDdXfMkmrHmnjYTjCU5WqRKoOW8_q-BUFhFc4PAQa00RwV890NEg-slqrFfSmt06DWoN7WM',
+};
+
+const reducer = (state, action) => {
+    console.log(action);
+    
+    switch(action.type) {
+        case 'SET_USER':
+            return {
+                ...state, 
+                user: action.user
+            };
+        case 'SET_TOKEN':
+            return {
+                ...state,
+                token: action.token,
+            } 
+        default: 
+            return state;
+    }
+};
+
+export default reducer;
