13b7e38dcba24a2305d89f062d37b24e12a8d180
const.kamil.lazarski@gmail.com <const.kamil.lazarski@gmail.com>
2020-08-20 01:00:32 +0200

    spotify web api connected with fetch token

--- primitive-spotify-clone/src/views/Root/Root.js
+++ primitive-spotify-clone/src/views/Root/Root.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Login from 'components/Login/Login';
+import Player from 'components/Player/Player'
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from 'theme/GlobalStyle';
 import theme from 'theme/theme';
+import { getTokenFromUrl } from 'api/spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+
+const spotify = new SpotifyWebApi();
 
 const Root = () => { 
-  return (  
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();  
+    window.location.hash = "";
+    const _token = hash.access_token;
+ 
+    if (_token) {
+      setToken(_token)
+
+      spotify.setAccessToken(_token);
+      spotify.getMe().then(user => {
+          console.log("hey ", user)
+      })
+    }
+  }, []);
+
+
+  return ( 
     <ThemeProvider theme={theme}>
-      <GlobalStyle />
-        <div className="app"> 
-          <Login />
+      <GlobalStyle />     
+        <div className="app">
+          { token ? <Player /> : <Login /> } 
         </div>
     </ThemeProvider> 
 )};
